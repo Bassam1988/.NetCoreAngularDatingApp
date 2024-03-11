5d0d2366480d5ffa6ed2a64b53bea47a8422669f
Bassam Suliman <bassam.suliman@forest-interactive.com>
2024-03-11 11:30:43 +0800

    call API by angular

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48c481b942c134a4928ea7ee56b75d60961e65d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5d17c038fe216d1fbfc68e66a6937aa5e419b5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
