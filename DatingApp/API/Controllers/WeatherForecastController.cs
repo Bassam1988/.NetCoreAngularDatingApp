e87e38fef19a844ad1fb62cd03559af4883f777c
Bassam Suliman <bassam.suliman@forest-interactive.com>
2024-03-13 16:52:44 +0800

    add DTO

--- DatingApp/API/Controllers/WeatherForecastController.cs
+++ DatingApp/API/Controllers/WeatherForecastController.cs
@@ -2,9 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+
+public class WeatherForecastController : BaseApiController
 {
     private static readonly string[] Summaries = new[]
     {
@@ -14,7 +13,7 @@ public class WeatherForecastController : ControllerBase
 
     public WeatherForecastController()
     {
-       
+
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d0d2366480d5ffa6ed2a64b53bea47a8422669f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8653aa8b043fcc1342fd8c63a0e725e8aea0034a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
